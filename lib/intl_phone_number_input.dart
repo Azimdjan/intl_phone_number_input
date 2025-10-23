7b7a882cf7a7c2d86fc5d3e43e64e51ac5649f6f
aotazhonov <aotazhonov@beeline.uz>
2025-10-24 03:18:45 +0500

    new exports

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -3,3 +3,6 @@ library intl_phone_number_input;
 export 'src/utils/phone_number.dart';
 export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
+export 'src/providers/country_provider.dart';
+export 'src/utils/formatter/as_you_type_formatter.dart';
+export 'src/utils/util.dart';
